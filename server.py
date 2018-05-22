d510a926bf6b6d5b862215f725de398fbd53cd24
Yuchen Huang <searchingundermoon@gmail.com>
2018-05-21 23:25:16 -0700

    Initialization of Project

---
+++ server.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, Response, url_for
+
+import socket
+
+import threading
+import sys
+import os
+import logging
+from stream import CameraStream
+import atexit
+
+app = Flask(__name__)
+
+# initialize the log system
+logger_format = '%(asctime)-30s %(levelname)-10s %(message)s'
+formatter = logging.Formatter(logger_format)
+hdlr = logging.FileHandler('./monitoring.log')
+hdlr.setFormatter(formatter)
+
+logging.basicConfig(format=logger_format)
+logger = logging.getLogger('monitoring')
+logger.setLevel(logging.INFO)
+logger.addHandler(hdlr)
+
+storage_directory = './storage/'
+
+
+def get_stream_from_remote(server_socket):
+    global output_manager
+    logger.info('Port is open, server is waiting for the remote steam.')
+    while True:
+        try:
+            # Accept a single connection and make a file-like object out of it
+            connection, addr = server_socket.accept()
+            logger.info('New stream comes: {}:{}'.format(addr[0], addr[1]))
+            # thread.start_new_thread(stream_receiver, (connection.makefile('rb'),))
+            camera_stream = CameraStream(
+                connection,
+                storage_directory,
+                30,
+                logger)
+            output_manager.append(camera_stream)
+        except Exception as e:
+            logger.warn('Failed to listen on port.' + str(e))
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            continue
+
+
+@app.route('/huang593')
+def index():
+    global output_manager
+    video_content = ""
+    i = 0
+    for camera_stream in output_manager:
+        if camera_stream.get_status() == 'Active':
+            video_content = video_content + """
+            <div class="row">
+                <div class="col-sm-12"></div>
+                <h3>{cam_name}</h3>
+                <img class="img-rounded img-responsive" style = "padding-left: 80px; padding-right: 80px;" src="{video_index}">
+            </div>
+            """.format(
+                cam_name=camera_stream.get_camera_name(),
+                video_index=url_for('video_feed', video_index=i))
+        i = i + 1
+
+    return render_template('index.html', video_content=video_content)
+
+
+def gen(video_index):
+    global output_manager
+    while True:
+        jpeg = output_manager[int(video_index)].image_output
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+
+
+@app.route('/video_feed<video_index>')
+def video_feed(video_index):
+    return Response(gen(video_index),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def global_initialization():
+    global output_manager
+    output_manager = list()
+
+
+def shutdown_hook():
+    """
+    a shutdown hook to be called before the shutdown
+    """
+    global server_socket
+    global thread_hdl
+    try:
+        server_socket.close()
+        if 'thread_hdl' in globals():
+            thread_hdl.stop()
+    finally:
+        logger.info('Shutdown')
+
+
+if __name__ == '__main__':
+    atexit.register(shutdown_hook)
+
+    # initialize environ ment
+    global_initialization()
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.listen(0)
+
+    global thread_hdl
+    thread_hdl = threading.Thread(target=get_stream_from_remote, args=(server_socket,))
+    thread_hdl.start()
+    # thread.start_new_thread(get_stream_from_remote, (server_socket,))
+    app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
---
+++ stream.py
@@ -0,0 +1,140 @@
+import time
+import os
+from PIL import Image
+import io
+import struct
+import pytz
+import numpy as np
+import cv2
+from datetime import datetime
+import threading
+
+
+class CameraStream:
+    # initialize some statistical variables
+    _bandwidth_calc_time = 0
+    _bandwidth_calc_count = 0
+    _bandwidth = 0
+    _frame_count = 0
+    _frame_per_second = 0
+
+    # sender
+    _sender_time_stamp = 0
+    _last_shotcut_time = 0
+    _shotcut_interval = 0
+
+    # initialize shotcut functions
+    _shotcut_directory = ''
+
+    _camera_name = ''
+    _connection = None
+    _last_update = 0
+
+    status = 'Inactive'
+
+    def __init__(self, connection, storage_directory, shotcut_interval, logger):
+        self._connection = connection
+        self._camera_name = struct.unpack(
+            '<50s',
+            connection.read(struct.calcsize('<50s')))[0].rstrip('\x00')
+        self._shotcut_directory = os.path.join(storage_directory, self._camera_name)
+        if not os.path.isdir(self._shotcut_directory):
+            os.makedirs(self._shotcut_directory)
+        self._last_update = time.time()
+        self._shotcut_interval = shotcut_interval
+        self.status = 'Active'
+        self._logger = logger
+        self._main_thread = threading.Thread(self.stream_receiver, (connection.makefile('rb'),))
+        self._main_thread.start()
+        logger.info('The stream from {} is initialized.'.format(self._camera_name))
+
+    def _put_text_on_image(self, target_text):
+        # turn the image into opencv compatible format
+        cv2.putText(
+            self._cv_image,
+            target_text,
+            (10, len(self._cv_image) - 20),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.3,
+            (255, 255, 255),
+            1
+        )
+
+    def _take_shotcut(self):
+        if time.time() - self._last_shotcut_time >= self.shotcut_interval:
+            label = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Eastern')).strftime('%y_%m_%d_%H_%M_%S')
+            output_path = self._shotcut_directory + '/' + label + '.jpg'
+            cv2.imwrite(output_path, self._cv_image)
+            self._logger.info('Output image to file: {}'.format(output_path))
+            self._last_shotcut_time = time.time()
+
+    def _stream_receiver(self):
+        try:
+            while True:
+                # Read the length of the image as a 32-bit unsigned int. If the
+                # length is zero, quit the loop
+                (sender_time_stamp, image_len) = struct.unpack(
+                    '<1dL',
+                    self._connection.read(struct.calcsize('<1dL'))
+                )
+
+                if not image_len:
+                    break
+                else:
+                    self._bandwidth_calc_count = self._bandwidth_calc_count + image_len
+                    self._frame_count = self._frame_count + 1
+
+                # Construct a stream to hold the image data and read the image
+                # data from the connection
+                image_stream = io.BytesIO()
+                image_stream.write(self._connection.read(image_len))
+
+                # Rewind the stream, open it as an image with PIL and do some
+                # processing on it
+                image_stream.seek(0)
+                self._image = Image.open(image_stream)
+                self._cv_image = np.array(self._image)
+
+                # calculate bandwidth
+                if (time.time() - self._bandwidth_calc_time) >= 2:
+                    bandwidth = self._bandwidth_calc_count / float(time.time() - self._bandwidth_calc_time) / 1000  # KB/S
+                    frame_per_second = self._frame_count / float(time.time() - self._bandwidth_calc_time)
+
+                    self._bandwidth_calc_time = time.time()
+                    self._bandwidth_calc_count = 0
+                    self._frame_count = 0
+
+                self._put_text_on_image("{.2f}kps\t{.2f}fps%s".format(
+                    bandwidth,
+                    frame_per_second,
+                    datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Eastern'))
+                ))
+
+                ret, self.image_output = cv2.imencode('.jpeg', self._cv_image, [cv2.IMWRITE_JPEG_QUALITY, 80])
+                self._last_update = time.time()
+                self._take_shotcut()
+
+        finally:
+            self._connection.close()
+            self.status = 'Inactive'
+
+    def get_status(self):
+        if time.time() - self._last_update > 60:
+            self.status = 'Inactive'
+        else:
+            return self.status
+
+    def get_camera_name(self):
+        return self._camera_name
+
+
+
+
+
+
+
+
+
+
+
+
